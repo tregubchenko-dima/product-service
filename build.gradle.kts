b1cc80274af8555db77175c27c2d66218ac734b5
tregubchenko2@yandex.ru <tregubchenko>
2025-06-07 10:43:43 +0300

    Initial

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,6 @@ dependencies {
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.testcontainers:junit-jupiter:1.20.2")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
